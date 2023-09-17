d7fd33dd8d73fe5a9fa2addabef1e56972b1aa30
Matheus Almeida <matheusalmeida016@gmail.com>
2023-09-17 17:37:56 -0400

    feat: add project setup

---
+++ src/app.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1 className="text-lg">Hello, world!</h1>
+}
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteSvgr from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsConfigPaths(), viteSvgr()],
+})
